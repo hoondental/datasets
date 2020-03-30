976c0df8beec37d158d3e94610c8be24d52506d7
hoondental <hoondental@gmail.com>
2020-03-30 12:40:05 +0900

    add in_memory

--- ljspeech/ljdataset.py
+++ ljspeech/ljdataset.py
@@ -22,64 +22,99 @@ from stts import audio, audio_util, util, textutil
 
 
 class LJDataset(Dataset):
-    def __init__(self, meta_path, _spec=True, _mel=True, _phone=False, stride=1, add_sos=False, add_eos=False):
-        self._spec = _spec
-        self._mel = _mel
-        self._phone = _phone
+    def __init__(self, meta_path, use_spec=True, use_mel=True, use_phone=False, stride=1, add_sos=False, add_eos=False, in_memory=False):
+        self.use_spec = use_spec
+        self.use_mel = use_mel
+        self.use_phone = use_phone
         self.stride = stride
         self.add_sos = add_sos
         self.add_eos = add_eos
+        self.in_memory = in_memory
         self.meta_dir = os.path.dirname(meta_path)
+        
+        self._script = []
+        self._text = []
+        self._phone1 = []
+        self._phone2 = []
+        self._mel = []
+        self._spec = []
+        self._n_text = []
+        self._n_phone = []
+        self._n_frame = []
         
         with open(meta_path, 'r') as f:
             lines = f.readlines()
         self.len = len(lines)
         self.meta = []
         for i, t in enumerate(lines):
-            fname, text, n_frame, spec_path, mel_path = t.split('|')
+            fname, text, spec_path, mel_path = t.split('|')
             n_frame = int(n_frame)
             if mel_path.endswith('\n'):
                 mel_path = mel_path[:-1]
-            self.meta.append((fname, text, n_frame, os.path.join(self.meta_dir, spec_path), os.path.join(self.meta_dir, mel_path)))
+            _spec_path = os.path.join(self.meta_dir, spec_path)
+            _mel_path = os.path.join(self.meta_dir, mel_path)
+            self.meta.append((fname, text, _spec_path, _mel_path))
             
+            _script = list(textutil.normalize(text))
+            _text = textutil.char2idx(_script)
+            if use_phone:
+                _phone = textutil.text2phone(_script)
+                _phone1 = textutil.phone2idx(_phone)
+                _phone2 = textutil.phone2idx2(_phone)
+            if self.add_sos:
+                _script = [textutil._char_vocab[1]] + _script
+                _text = [1] + _text 
+                if use_phone:
+                    _phone = [textutil._char_vocab[1]] + _phone
+                    _phone1 = [1] + _phone1
+                    _phone2 = [(1, 0)] + _phone2
+            if self.add_eos:
+                _script = [textutil._char_vocab[2]] + _script
+                _text = [2] + _text 
+                if use_phone:
+                    _phone = [textutil._char_vocab[2]] + _phone
+                    _phone1 = [2] + _phone1
+                    _phone2 = [(2, 0)] + _phone2
+            self._script.append(_script)
+            self._text.append(_text)
+            self._n_text.append(len(_text))
+            if use_phone:
+                self._phone.append(_phone)
+                self._phone1.append(_phone1)
+                self._phone2.append(_phone2)
+                self._n_phone.append(len(_phone))
+            if in_memory:
+                _n_frame = 0
+                if use_spec:
+                    _spec = np.load(_spec_path)[...,::self.stride]
+                    self._spec.append(_spec) 
+                    _n_frame = _spec.shape[-1]
+                if use_mel:
+                    _mel = np.load(_mel_path)[...,::self.stride]
+                    self._mel.append(_mel) 
+                    _n_frame = _mel.shape[-1]
+                self._n_frame.append(_n_frame)           
             
     def __len__(self):
         return self.len
     
     def __getitem__(self, idx):
         meta = self.meta[idx]
-        spec_path = meta[3]
-        mel_path = meta[4]
-        _text = meta[1]
-        n_frame = meta[2]
-        _text = textutil.text_normalize(_text)
-        text = textutil.char2idx(_text)
-        if self._phone:
-            _phone = textutil.text2phone(_text)
-            phone1 = textutil.phone2idx(_phone)
-            phone2 = textutil.phone2idx2(_phone)
-        if self.add_sos:
-            text = [1] + text 
-            if self._phone:
-                phone1 = [1] + phone1
-                phone2 = [(1, 0)] + phone2
-        if self.add_eos:
-            text = text + [2]
-            if self._phone:
-                phone1 = phone1 + [2]
-                phone2 = phone2 + [(2, 0)]
-        
-        text = np.array(text, dtype=np.int64)
-        sample = {'idx':idx, 'text':text, 'n_text':len(text), 'n_frame':math.ceil(n_frame/self.stride)}
+        _spec_path = meta[3]
+        _mel_path = meta[4]
+        _text = self._text[idx]
+
+        text = np.array(_text, dtype=np.int64)
+        sample = {'idx':idx, 'text':text, 'n_text':self._n_text[idx], 'n_frame':self._n_frame[idx]}
         if self._spec:
-            sample['spec'] = np.load(spec_path)[...,::self.stride]
+            sample['spec'] = self._spec[idx] if self.in_memory else np.load(spec_path)[...,::self.stride]
         if self._mel:
-            sample['mel'] = np.load(mel_path)[...,::self.stride]        
+            sample['mel'] = self._mel[idx] if self.in_memory else np.load(mel_path)[...,::self.stride]        
         if self._phone:
-            sample['phone1'] = np.array(phone1, dtype=np.int64)
-            sample['phone2'] = np.array(phone2, dtype=np.int64).T    
-            sample['n_phone1'] = len(phone1)
-            sample['n_phone2'] = len(phone2)
+            sample['phone1'] = np.array(self._phone1[idx], dtype=np.int64)
+            sample['phone2'] = np.array(self._phone2[idx], dtype=np.int64).T    
+            sample['n_phone1'] = self._n_phone[idx]
+            sample['n_phone2'] = self._n_phone[idx]
         return sample
             
     def collate(self, samples):
--- ljspeech/preprocess.py
+++ ljspeech/preprocess.py
@@ -114,7 +114,7 @@ def process_wavfiles(meta, out_dir, sample_rate=22050, trim_db=None, mono=True, 
                                            spec_paths, None, mel_paths, decibel, normalize)
     with open(meta_path, 'w') as f: 
         for i, m in enumerate(_meta):
-            f.write(m[0] + '|' + m[1] + '|' + str(n_frames[i]) + '|' + m[2] + '|' + m[3] + '\n')
+            f.write(m[0] + '|' + m[1] + '|' + m[2] + '|' + m[3] + '\n')
             
     with open(os.path.join(out_dir, 'settings.txt'), 'w') as f:
         f.write('sample_rate:' + str(sample_rate) + '\n')
