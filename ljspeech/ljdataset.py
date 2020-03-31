f82844ae6033b84ffde7b5377472791020c75502
hoondental <hoondental@gmail.com>
2020-03-31 19:49:04 +0900

    relative import

--- ljspeech/ljdataset.py
+++ ljspeech/ljdataset.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.dirname(__file__))
+#sys.path.append(os.path.dirname(__file__))
 #sys.path.append('..')
 #sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 import math
@@ -17,7 +17,7 @@ import torch, torch.nn as nn, torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 
 
-from stts import audio, audio_util, util, textutil
+from .stts import audio, audio_util, util, textutil
 
 
 
