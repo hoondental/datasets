48c651192b6deca736d758fb873b2b29cd8f8a95
hoondental <hoondental@gmail.com>
2020-07-15 22:46:51 +0900

    added clovacall

---
+++ clovacall/clovacall_dataset.py
@@ -0,0 +1,217 @@
+import os
+import sys
+#sys.path.append(os.path.dirname(__file__))
+#sys.path.append('..')
+#sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+import math
+import numpy as np
+from scipy.signal import get_window
+import librosa.util as librosa_util
+import librosa
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+from tqdm import tqdm
+from shutil import copyfile
+
+import torch, torch.nn as nn, torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.sampler import Sampler
+import random
+
+import tensorflow as tf
+
+
+if __package__ == '':
+    from stts import audio, audio_util, util, textutil
+else:
+    from .stts import audio, audio_util, util, textutil
+
+
+
+class ClovaCallDataset(Dataset):
+    def __init__(self, meta_path, use_spec=True, use_mel=False, stride=1, add_sos=False, add_eos=False, in_memory=False,
+                 include_numbers=True, tensor_type='torch'):
+        self.use_spec = use_spec
+        self.use_mel = use_mel
+        self.stride = stride
+        
+        self.include_numbers = include_numbers
+        if include_numbers:
+            self._char_vocab = textutil._knchar_vocab
+            self.char2idx = textutil.knchar2idx
+        else:
+            self._char_vocab = textutil._kchar_vocab
+            self.char2idx = textutil.kchar2idx
+        
+        self.add_sos = add_sos
+        self.add_eos = add_eos
+        self.pad = self._char_vocab[0]
+        self.sos = self.._char_vocab[1]
+        self.eos = self._char_vocab[2]
+        
+        self.tensor_type = tensor_type
+        self.in_memory = in_memory
+        self.meta_dir = os.path.dirname(meta_path)
+        self.meta_path = meta_path
+        
+        self.meta = []
+        self._script = []
+        self._text = []
+        self._mel = []
+        self._spec = []
+        self._mel_path = []
+        self._spec_path = []
+        self._n_frame = []
+        
+        with open(meta_path, 'r') as f:
+            lines = f.readlines()
+        self.len = len(lines)
+        
+        for i, t in enumerate(lines):
+            fname, text, n_frame, spec_path, mel_path = t.split('|')
+            if mel_path.endswith('\n'):
+                mel_path = mel_path[:-1]
+            n_frame = int(n_frame)
+            self.meta.append((fname, text, n_frame, spec_path, mel_path))
+            
+            _spec_path = os.path.join(self.meta_dir, spec_path)
+            _mel_path = os.path.join(self.meta_dir, mel_path)
+            self._spec_path.append(_spec_path)
+            self._mel_path.append(_mel_path)
+            self._n_frame.append(n_frame)
+            
+#            _script = textutil.text_normalize(text)
+            _text = self.char2idx(_script)
+
+            if self.add_sos:
+                _script = self.sos + _script
+                _text = [1] + _text 
+            if self.add_eos:
+                _script = self.eos + _script
+                _text = [2] + _text 
+            self._script.append(_script)
+            self._text.append(_text)
+            if in_memory:
+                if use_spec:
+                    _spec = np.load(_spec_path)[...,::self.stride]
+                    self._spec.append(_spec) 
+                if use_mel:
+                    _mel = np.load(_mel_path)[...,::self.stride]
+                    self._mel.append(_mel)         
+            
+    def __len__(self):
+        return self.len
+    
+    def __getitem__(self, idx):
+        _spec_path = self._spec_path[idx]
+        _mel_path = self._mel_path[idx]
+        _text = self._text[idx]
+
+        _text = np.array(_text, dtype=np.int64)
+        _n_frame = 0
+        sample = {'idx':idx, 'text':_text, 'n_text':len(_text)}
+        if self.use_spec:
+            _spec = self._spec[idx] if self.in_memory else np.load(_spec_path)[...,::self.stride]
+            _n_frame = _spec.shape[-1]
+            sample['spec'] = _spec
+        if self.use_mel:
+            _mel = self._mel[idx] if self.in_memory else np.load(_mel_path)[...,::self.stride]        
+            _n_frame = _mel.shape[-1]
+            sample['mel'] = _mel
+        sample['n_frame'] = _n_frame
+        return sample
+            
+    def collate(self, samples):
+        text_lengths = []
+        n_phones1 = []
+        n_phones2 = []
+        n_frames = []
+        idxes = []
+        texts = []
+        specs = []
+        mels = []
+        phones1 = []
+        phones2 = []
+        for i, s in enumerate(samples):
+            text_lengths.append(s['n_text'])
+            n_frames.append(s['n_frame'])
+            idxes.append(s['idx'])
+            if self.use_phone:
+                n_phones1.append(s['n_phone1'])
+                n_phones2.append(s['n_phone2'])
+        max_text_len = max(text_lengths)
+        max_n_frame = max(n_frames)
+        
+        for i, s in enumerate(samples):
+            texts.append(np.pad(s['text'], (0, max_text_len - text_lengths[i]), constant_values=0))
+            if self.use_spec:
+                specs.append(np.pad(s['spec'], ((0, 0), (0, max_n_frame - n_frames[i])), constant_values=0.0))
+            if self.use_mel:
+                mels.append(np.pad(s['mel'], ((0, 0), (0, max_n_frame - n_frames[i])), constant_values=0.0))    
+                    
+        if self.tensor_type == 'torch':
+            tensor = torch.tensor
+            int32 = torch.int32
+            int64 = torch.int64
+            float32 = torch.float32
+        elif self.tensor_type == 'numpy':
+            tensor = np.array
+            int32 = np.int32
+            int64 = np.int64
+            float32 = np.float32
+        else:
+            raise Exception('only torch or numpy is supported')
+            
+        batch = {'idx':tensor(idxes, dtype=int64), 
+                 'text':tensor(texts, dtype=int64), 
+                 'n_text':tensor(text_lengths, dtype=int32),
+                 'n_frame':tensor(n_frames, dtype=int32)}
+        if self.use_spec:
+            batch['spec'] = tensor(specs, dtype=float32)
+        if self.use_mel:
+            batch['mel'] = tensor(mels, dtype=float32)
+        return batch
+    
+    def get_length_sampler(self, batch_size, noise=10.0, shuffle=True):
+        sampler = LengthSampler(self._n_frame, batch_size, noise, shuffle)
+        return sampler
+        
+        
+class LengthSampler(Sampler):
+    def __init__(self, lengths, batch_size, noise=10.0, shuffle=True):
+        self.lengths = lengths
+        self.batch_size = batch_size
+        self.noise = noise
+        self.shuffle = shuffle
+        
+    def __len__(self):
+        return len(self.lengths)
+    
+    def __iter__(self):
+        _lengths = torch.tensor(self.lengths, dtype=torch.float32)
+        _lengths = _lengths + 2 * self.noise * torch.rand_like(_lengths)
+        _sorted, _idx = torch.sort(_lengths)
+        
+        if self.shuffle:
+            _len = len(_lengths)
+            _num_full_batches = _len // self.batch_size
+            _full_batch_size = _num_full_batches * self.batch_size
+            _full_batch_idx = _idx[:_full_batch_size]
+            _remnant_idx = _idx[_full_batch_size:]
+            _rand_batch_idx = torch.randperm(_num_full_batches)
+            _rand_full_idx = _full_batch_idx.reshape(_num_full_batches, self.batch_size)[_rand_batch_idx].reshape(-1) 
+            _rand_idx = torch.cat([_rand_full_idx, _remnant_idx], dim=0)
+            return iter(list(_rand_idx.numpy()))
+        else:
+            return iter(list(_idx.numpy()))
+
+
+
+
+
+
+
+
+
+    
+    
---
+++ clovacall/preprocess.py
@@ -0,0 +1,164 @@
+import os
+import sys
+#sys.path.append(os.path.dirname(__file__))
+#sys.path.append('..')
+#sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+import math
+import numpy as np
+from scipy.signal import get_window
+import librosa.util as librosa_util
+import librosa
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+from tqdm import tqdm
+from shutil import copyfile
+import argparse
+
+import torch, torch.nn as nn, torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+if __package__ == '':
+    from stts import audio, audio_util, util, textutil
+else:
+    from .stts import audio, audio_util, util, textutil
+
+def main():
+    parser = argparse.ArgumentParser(description='stft ClovaCall dataset to mel and linear spectrogram')
+    parser.add_argument('--data_dir', '-d')
+    parser.add_argument('--out_dir', '-o')
+    parser.add_argument('--sample_rate', '-s', type=int, default=22050)
+    parser.add_argument('--n_fft', '-f', type=int, default=1024)
+    parser.add_argument('--win_length', '-w', type=int, default=1024)
+    parser.add_argument('--hop_length', '-p', type=int, default=256)
+    parser.add_argument('--n_mels', '-m', type=int, default=80)
+    parser.add_argument('--mono', type=bool, default=True)
+    parser.add_argument('--trim_db', type=int, default=None)
+    parser.add_argument('--decibel', type=bool, default=True)
+    parser.add_argument('--normalize', type=bool, default=True)
+    parser.add_argument('--num_vals', type=int, default=3100)
+    args = parser.parse_args()
+    
+    preprocess(args.data_dir, args.out_dir, args.num_vals, args.sample_rate, args.n_fft, args.win_length, args.hop_length, 
+               args.n_mels, args.mono, args.trim_db, args.decibel, args.normalize)
+
+def make_metafile(dir_base, subdir_wav='wavs', metafile='meta.txt'):
+    meta = []
+    dir_wavs = os.path.join(dir_base, subdir_wav)
+    for a, b, c in os.walk(dir_wavs):
+        if len(c) == 0:
+            continue
+        for fname in c:
+            if 'csv' in fname:
+                fpath = os.path.join(a, fname)
+                with open(fpath, 'r') as f:
+                    lines = f.readlines()
+                    for line in lines:
+                        parts = line.strip().split(',')
+                        if len(parts) != 2:
+                            continue
+                        _meta = (os.path.join(a[len(dir_clova)+1:], parts[0].strip()), parts[1].strip())
+                        meta.append(_meta)        
+    with open(os.path.join(dir_clova, metafile), 'w') as f:
+        lines = ''
+        for _meta in meta:
+            lines += '|'.join(_meta) + '\n'
+        f.write(lines[:-1])
+    return meta
+
+
+def seperate_train_val(meta, dir_base, val_ratio=0.2, train_file='train_meta.txt', val_file='val_meta.txt'):
+    _len = len(meta)
+    n_val = int(_len * val_ratio)
+    n_train = _len - n_val
+    train_meta = []
+    val_meta = []
+    _train_index = np.random.choice(_len, n_train, replace=False)
+    for i in range(_len):
+        if i in _train_index:
+            train_meta.append(meta[i])
+        else:
+            val_meta.append(meta[i])
+    with open(os.path.join(dir_base, train_file), 'w') as f:
+        lines = ''
+        for _meta in train_meta:
+            lines += '|'.join(_meta) + '\n'
+        f.write(lines[:-1])
+    with open(os.path.join(dir_base, val_file), 'w') as f:
+        lines = ''
+        for _meta in val_meta:
+            lines += '|'.join(_meta) + '\n'
+        f.write(lines[:-1])
+        
+    
+def preprocess(data_dir, metafile, out_subdir='train', sample_rate=22050, n_fft=1024, win_length=None, 
+               hop_length=None, n_mels=80, mono=True, trim_db=None, decibel=True, normalize=True):
+    meta = read_metadata(data_dir, metafile)
+    out_dir = os.path.join(data_dir, out_subdir)
+    process_wavfiles(meta, out_dir, sample_rate, trim_db, mono, n_fft, win_length, hop_length, n_mels, decibel, normalize)
+    
+def read_metadata(data_dir, metafile='meta.txt'):    
+    metadata_path = os.path.join(data_dir, metafile)
+    with open(metadata_path, 'r') as f:
+        lines = f.readlines()
+    meta = []
+    for line in lines:
+        wpath, ntext = line.strip().split('|')
+        if ntext.endswith('\n'):
+            ntext = ntext[:-1]
+        fname = wpath.split('/')[-1][:-4]
+        meta.append((fname, os.path.join(data_dir, wpath), ntext))
+    return meta
+
+
+def process_wavfiles(meta, out_dir, sample_rate=22050, trim_db=None, mono=True, n_fft=1024, win_length=None, hop_length=None, 
+                     n_mels=80, decibel=True, normalize=True):
+    if win_length is None:
+        win_length = n_fft
+    if hop_length is None:
+        hop_length = int(win_length / 4)
+    _spec_dir = 'spec'
+    _mel_dir = 'mel'
+    spec_dir = os.path.join(out_dir, _spec_dir)
+    mel_dir = os.path.join(out_dir, _mel_dir)
+    meta_path = os.path.join(out_dir, 'meta.txt')
+   
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+    if not os.path.exists(spec_dir):
+        os.mkdir(spec_dir)
+    if not os.path.exists(mel_dir):
+        os.mkdir(mel_dir)
+    wav_paths = []
+    spec_paths = []
+    mel_paths = []
+    _meta = []
+    for fname, path, ntext in meta:
+        wav_paths.append(path)
+        spec_file = fname + '.spec.npy'
+        mel_file = fname + '.mel.npy'
+        spec_path = os.path.join(spec_dir, spec_file)
+        mel_path = os.path.join(mel_dir, mel_file)
+        spec_paths.append(spec_path)
+        mel_paths.append(mel_path)
+        _meta.append((fname, ntext, os.path.join(_spec_dir, spec_file), os.path.join(_mel_dir, mel_file)))
+                     
+    n_frames = audio_util.wav_to_spec_save_many(wav_paths, sample_rate, trim_db, mono, n_fft, win_length, hop_length, n_mels, 
+                                           spec_paths, None, mel_paths, decibel, normalize)
+    with open(meta_path, 'w') as f: 
+        for i, m in enumerate(_meta):
+            f.write(m[0] + '|' + m[1] + '|' + str(n_frames[i]) + '|' + m[2] + '|' + m[3] + '\n')
+            
+    with open(os.path.join(out_dir, 'settings.txt'), 'w') as f:
+        f.write('sample_rate:' + str(sample_rate) + '\n')
+        f.write('n_fft:' + str(n_fft) + '\n')
+        f.write('win_length:' + str(win_length) + '\n')
+        f.write('hop_length:' + str(hop_length) + '\n')
+        f.write('n_mels:' + str(n_mels) + '\n')
+        f.write('trim_db:' + str(trim_db) + '\n')
+        f.write('mono:' + str(mono) + '\n')
+        f.write('decibel:' + str(decibel) + '\n')
+        f.write('normalize:' + str(normalize) + '\n')
+    
+if __name__ == '__main__':
+    main()
+            
